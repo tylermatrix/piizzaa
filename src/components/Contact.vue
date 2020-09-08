dbed00febe713009c94814a7486e17fa468a46f0
tylermatrix <tylermaatrix@gmail.com>
2020-09-08 17:21:48 -0400

    added base components

---
+++ src/components/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <h5 class="card-title">Contact</h5>
+      <p class="card-text">
+      <address>
+          <strong>Piizzaa</strong><br>
+          Address line 1 <br>
+          Address line 2 <br>
+          Phone: (123) 123-1231
+      </address>
+      <address>
+          <strong>Email:</strong><br>
+          <p>contact@piizzaa.com</p>
+      </address>
+      
+    </div>
+  </div>
+</template>
---
+++ src/components/OrderingGuide.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <h5 class="card-title">How to order</h5>
+      <p
+        class="card-text"
+      >
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque debitis culpa fuga nemo amet eum sed laborum beatae labore similique!
+      </p>
+    </div>
+  </div>
+</template>
