bac727b05b12dccffb87c488cf3a13f45827100c
tylermatrix <tylermaatrix@gmail.com>
2020-09-07 20:47:34 -0400

    created menu component

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,25 +1,130 @@
 <template>
-    <div class="row">
+  <div class="row">
+      <div class="col-sm-12 col-md-6">
+          <table class="table table-hover text-center">
+              <thead class="thead-default">
+                  <tr>
+                      <th>Size</th>
+                      <th>Price</th>
+                      <th>Add to basket</th>
+                  </tr>
+              </thead>
+              <tbody v-for="item in getMenuItem" :key="item.name">
+                  <tr>
+                      <td><strong>{{item.name}}</strong></td>
+                  </tr>
+                  <tr v-for="option in item.options" :key="option.size">
+                      <td>{{option.size}}</td>
+                      <td>{{option.price}}</td>
+                      <td><button class="btn btn-sm btn-outline-success" 
+                      type="button"
+                      @click="addToBasket(item, option)">+</button></td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+      
         <div class="col-sm-12 col-md-6">
-            <table class="table table-hover">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Size</th>
-                        <th>Price</th>
-                        <th>Add to basket</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><strong>Margherita</strong></td>
-                    </tr>
-                    <tr>
-                        <td>9"</td>
-                        <td>$12.95</td>
-                        <td><button class="btn btn-sm btn-outline-success" type="button">+</button></td>
-                    </tr>
-                </tbody>
-            </table>
+            <div v-if="basket.length > 0">
+                <table class="table text-center">
+                    <thead class="thead-default">
+                        <tr>
+                            <th>Quantity</th>
+                            <th>Item</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody v-for="item in basket" :key="item">
+                        <tr>
+                            <td>
+                                <button class="btn btn-sm" 
+                                type="button"
+                                @click="decreaseQuantity(item)">-</button>
+                                <span>{{item.quantity}}-</span>
+                                <button class="btn btn-sm" 
+                                type="button"
+                                @click="increaseQuantity(item)">+</button>
+                            </td>
+                            <td>{{item.name}} {{item.size}}</td>
+                            <td>{{item.price * item.quantity}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>Order total: </p>
+                <button class="btn btn-success btn-block">Place Order</button>
+            </div>
+            <div v-else>
+                <p>{{basketText}}</p>
+            </div>
         </div>
-    </div>
+
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+      return {
+          basket: [],
+          basketText: "Your basket is empty!",
+          getMenuItem: {
+            1: {
+            'name': 'Margherita',
+            'description': 'A delicious tomato based pizza topped with mozzarella',
+            'options': [{
+                'size': 9,
+                'price': 6.95
+            }, {
+                'size': 12,
+                'price': 10.95
+            }]
+            },
+            2: {
+            'name': 'Pepperoni',
+            'description': 'A delicious tomato based pizza topped with mozzarella and pepperoni',
+            'options': [{
+                'size': 9,
+                'price': 7.95
+            }, {
+                'size': 12,
+                'price': 12.95
+            }]
+            },
+            3: {
+            'name': 'Ham and Pineapple',
+            'description': 'A delicious tomato based pizza topped with mozzarella, ham and pineapple',
+            'options': [{
+                'size': 9,
+                'price': 7.95
+            }, {
+                'size': 12,
+                'price': 12.95
+            }]
+            }
+          }
+      }
+  },
+  methods: {
+      addToBasket(item, option) {
+            this.basket.push({
+             name: item.name,
+             price: option.price,
+             size: option.size,
+             quantity: 1
+         }) 
+      },
+      decreaseQuantity(item) {
+          item.quantity--;
+          if(item.quantity <= 0) {
+              this.removeFromBasket(item);
+          }
+      },
+      increaseQuantity(item) {
+          item.quantity++;
+      },
+      removeFromBasket(item) {
+          this.basket.splice(this.basket.indexOf(item), 1);
+      }
+  },
+}
+</script>
