2169b436f623c88f46ad75ae1b73148656ec94ec
tylermatrix <tylermaatrix@gmail.com>
2020-09-08 14:41:20 -0400

    implemented login/logout auth

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,8 +10,42 @@
                 <input id="password"  placeholder= "Enter Password" type="password" class="form-control">
             </div>
 
-            <button type="button" class="btn btn-primary">Sign In</button>
-            <button type="button" class="btn btn-danger">Sign Out</button>
+            <button type="button" class="btn btn-primary" @click.prevent="signIn">Sign In</button>
+            <button type="button" class="btn btn-danger" @click.prevent="signOut">Sign Out</button>
         </form>
     </div>
 </template>
+
+<script>
+import Firebase from 'firebase'
+
+
+export default {
+    methods: {
+        signIn(){
+            var email = document.getElementById('email').value
+            var password = document.getElementById('password').value
+
+            Firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                if(errorCode == 'auth/wrong-password') {
+                    alert('Wrong password')
+                } else {
+                    alert(errorMessage)
+                } 
+                
+            }
+
+            )   },
+        signOut() {
+            Firebase.auth().signOut().then(function() {
+                alert('Logged out')
+            }).catch(function(error) {
+                alert('error')
+            })
+        }
+    }
+}
+</script>
